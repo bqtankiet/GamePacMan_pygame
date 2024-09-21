c487b5b3652dcb581efce111b228c83fb0566614
Tan Kiet <bqtankiet@gmail.com>
2024-09-21 23:21:10 +0700

    add method text_image

--- src/utils/spriteLoader.py
+++ src/utils/spriteLoader.py
@@ -14,9 +14,10 @@ class SpriteLoader:
 
     def __init__(self) -> None:
         if not hasattr(self, 'initialized'):
-            self.general_sprites = pygame.image.load(RESOURCE + "general_sprites.png")
-            self.scene = pygame.image.load(RESOURCE + "scene.png")
+            self.general_sprites = pygame.image.load(RESOURCE + "Arcade - Pac-Man - General Sprites.png")
+            self.scene = pygame.image.load(RESOURCE + "Arcade - Pac-Man - Attract Mode & HUD Assets.png")
             self.text = pygame.image.load(RESOURCE + "text.png")
+            self.all_text = pygame.image.load(RESOURCE + "Arcade - Pac-Man - Text.png")
             self.initialized = True
     
     def background_main_menu (self):
@@ -44,3 +45,29 @@ class SpriteLoader:
         img = pygame.transform.scale_by(img, SCALE)
         return img
 
+    def text_image(self, text, color = 'white'):
+        text = text.upper()
+        text_surface = pygame.Surface((len(text)*8, 8))
+        index = 0
+        for char in text:
+            if char == " ": 
+                index+=1
+                continue
+
+            if(char in "ABCDEFGHIJKLMNO"):  pos = {'line': 0, 'index': ord(char) - ord('A')}
+            if(char in "PQRSTUVWXYZ"):      pos = {'line': 1, 'index': ord(char) - ord('P')}
+            if(char in "0123456789"):       pos = {'line': 2, 'index': ord(char) - ord('0')}
+            if(color.lower() == "red"):     pos["line"] += 4
+            if(color.lower() == "pink"):    pos["line"] += 4*2
+            if(color.lower() == "cyan"):    pos["line"] += 4*3
+            if(color.lower() == "orange"):  pos["line"] += 4*4
+            if(color.lower() == "yellow"):  pos["line"] += 4*6
+
+            left_top = (pos["index"]*8, pos["line"]*8)
+            char_img = self.all_text.subsurface(left_top, (8, 8))
+            text_surface.blit(char_img, (index*8, 0))
+            index += 1
+
+        text_surface = pygame.transform.scale_by(text_surface, SCALE)
+        return text_surface
+    
