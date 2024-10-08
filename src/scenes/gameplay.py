94518bcbdf7ce32a2b2da4c73d9abc8dc9a4e486
Tan Kiet <bqtankiet@gmail.com>
2024-10-08 23:17:30 +0700

    create first simple gameplay scene

--- src/scenes/gameplay.py
+++ src/scenes/gameplay.py
@@ -1,11 +1,33 @@
+import pygame
+
+from src.entities.pacman import Pacman
 from src.scenes.scene import Scene
 
 
 class GamePlay(Scene):
-    def render_surface(self): pass
+    def __init__(self, game):
+        super().__init__(game)
+        self.pacman = Pacman()
+        self.sprites = pygame.sprite.Group()
+        self.sprites.add(self.pacman)
 
-    def handle_event(self): pass
+    def render_surface(self):
+        self.surface.fill("BLACK")
+        self.sprites.draw(self.surface)
 
-    def update(self): pass
+    def handle_event(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.pacman.direction = "left"
+        elif keys[pygame.K_RIGHT]:
+            self.pacman.direction = "right"
+        elif keys[pygame.K_UP]:
+            self.pacman.direction = "up"
+        elif keys[pygame.K_DOWN]:
+            self.pacman.direction = "down"
+
+    def update(self):
+        self.sprites.update()
+        self.render_surface()
 
     def reset(self): pass
